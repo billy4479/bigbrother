b818d6a5316d308082d0beaea742fad519e0eabf
billy4479 <giachi.ellero@gmail.com>
2022-10-24 09:15:11 +0200

    server: setup simple forwarding of messages from ui to client

--- server/clientSocket.go
+++ server/clientSocket.go
@@ -7,6 +7,12 @@ import (
 
 var upgrader = websocket.Upgrader{}
 
+type Client struct {
+	writeChan chan message
+}
+
+var client *Client
+
 func clientSocket(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
@@ -14,11 +20,52 @@ func clientSocket(c echo.Context) error {
 	}
 	defer ws.Close()
 
-	for {
-		// TODO
-		break
-	}
+	readChan := make(chan message)
+	errorChan := make(chan error)
 
-	return nil
+	go func() {
+		for {
+			msgType, buffer, err := ws.ReadMessage()
+			if err != nil {
+				errorChan <- err
+			} else {
+				readChan <- message{msgType: msgType, buffer: buffer}
+			}
+		}
+	}()
+
+	client = &Client{
+		writeChan: make(chan message),
+	}
+	defer func() {
+		close(client.writeChan)
+		client = nil
+	}()
+
+	for {
+		select {
+		case message := <-readChan:
+			if ui != nil {
+				ui.writeChan <- message
+			}
+
+			if message.msgType == websocket.TextMessage {
+				if string(message.buffer) == "clientDisconnect" {
+					return nil
+				}
+			}
+
+		case message := <-client.writeChan:
+			errorChan <- ws.WriteMessage(message.msgType, message.buffer)
+
+		case err := <-errorChan:
+			if err != nil {
+				ws.Close()
+				close(readChan)
+				close(errorChan)
+				return err
+			}
+		}
+	}
 
 }
--- server/uiSocket.go
+++ server/uiSocket.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
+
+type message struct {
+	msgType int
+	buffer  []byte
+}
+
+type UI struct {
+	writeChan chan message
+}
+
+var ui *UI = nil
 
 func uiSocket(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
@@ -11,11 +23,52 @@ func uiSocket(c echo.Context) error {
 	}
 	defer ws.Close()
 
-	for {
-		// TODO
-		break
-	}
+	readChan := make(chan message)
+	errorChan := make(chan error)
 
-	return nil
+	go func() {
+		for {
+			msgType, buffer, err := ws.ReadMessage()
+			if err != nil {
+				errorChan <- err
+			} else {
+				readChan <- message{msgType: msgType, buffer: buffer}
+			}
+		}
+	}()
+
+	ui = &UI{
+		writeChan: make(chan message),
+	}
+	defer func() {
+		close(ui.writeChan)
+		ui = nil
+
+		close(readChan)
+		close(errorChan)
+		ws.Close()
+	}()
+
+	for {
+		select {
+		case message := <-readChan:
+			if client != nil {
+				client.writeChan <- message
+			}
+			if message.msgType == websocket.TextMessage {
+				if string(message.buffer) == "uiDisconnect" {
+					return nil
+				}
+			}
+
+		case message := <-ui.writeChan:
+			errorChan <- ws.WriteMessage(message.msgType, message.buffer)
+
+		case err := <-errorChan:
+			if err != nil {
+				return err
+			}
+		}
+	}
 
 }
